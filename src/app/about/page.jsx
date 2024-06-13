b616a02c4d33c991ae31c1a53b80aebcbeda3f6e
erDeep640390 <erdeep64930@gmail.com>
2024-06-13 20:35:34 +0530

    about us page has ben added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-600 text-center'>About Us</h1>
+            <p>Study of Society that focuses on discovering the meanings that
+                people attach to their social world. (Individuals have a voluntary will and his/her
+                thoughts cannot be understood simply in terms of external influence).
+                They are concerned With explaining social
+                actions in terms of meanings that Individuals give to them. However, they focus on
+                small-scale interactions rather than large scale social change.
+                Approach in sociology that aims to reveal what role human
+                awareness plays in the production of social action, social situations and social
+                worlds. In essence it believes that society is a human construction</p>
+        </div>
+    )
+}
+
+export default page
