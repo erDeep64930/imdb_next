cef1998ceb919b2ca56a0f67c914d4da52a58b70
erDeep640390 <erdeep64930@gmail.com>
2024-06-14 10:04:33 +0530

    underline in dynamic route has been corrected and data has been fetch correctly

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const url = genre === "fetchTopRated"
+    ? `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    : `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  console.log(results);
+
   return (
-  <div> </div>
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h2>{result.title || result.name}</h2>
+          <p>{result.overview}</p>
+        </div>
+      ))}
+    </div>
   );
 }
